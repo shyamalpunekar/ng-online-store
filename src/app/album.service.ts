ea623b860b303784c0c70b74ce3fdf2f582f0dcc
shy <shyamal.punekar@gmail.com>
2017-09-27 12:14:04 -0700

    Add services to display album details

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-//import { ALBUMS } from './mock-albums';
+import { ALBUMS } from './mock-albums';
 
 @Injectable()
 export class AlbumService {
 
   constructor() { }
-  // getAlbums() {
-  //   return ALBUMS;
-  // }
+  getAlbums() {
+    return ALBUMS;
+  }
+
+  getAlbumById(albumId: number){
+    for(var i=0; i<= ALBUMS.length - 1; i++){
+      if(ALBUMS[i].id === albumId){
+        return ALBUMS[i];
+      }
+    }
+
+  }
 
 }
